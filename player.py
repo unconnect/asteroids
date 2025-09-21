6576ae8f3966b14dd6fd4cb174d66bc7ff17071d
Alexander Nikolas Reuber <info@unconnect.net>
2025-09-21 23:09:25 +0200

    add player movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, COLOR_WHITE, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, COLOR_WHITE, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -29,7 +29,16 @@ class Player(CircleShape):
   def update(self, dt):
     keys = pygame.key.get_pressed()
 
+    # Classic WASD movement
     if keys[pygame.K_a]:
-        self.rotate(dt)
+      self.rotate(dt)
     if keys[pygame.K_d]:
-        self.rotate(-dt)
+      self.rotate(-dt)
+    if keys[pygame.K_w]:
+      self.move(dt)
+    if keys[pygame.K_s]:
+      self.move(-dt)
+  
+  def move(self, dt):
+     forward = pygame.Vector2(0, 1).rotate(self.rotation)
+     self.position += forward * PLAYER_SPEED * dt
