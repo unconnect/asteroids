2d0cc2f98b572be5dbf1ccb71e5881934c838397
Alexander Nikolas Reuber <info@unconnect.net>
2025-09-21 22:27:33 +0200

    add game clock and tick the gameloop for a 60 fps refreshrate

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import pygame
 from constants import *
 
 def main():
+    # Init the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # The gameloop
+    # Game clock
+    clock = pygame.time.Clock()
+    # Delta time
+    dt = 0
+
+    # Start the gameloop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,7 +20,8 @@ def main():
         screen.fill(BACKGROUND_COLOR)
         # Update the screen
         pygame.display.flip()
-
+        # Update delta time by seconds 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
